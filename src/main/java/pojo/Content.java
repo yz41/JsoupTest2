095213d262001f7f4f5a395e21c9746ae78c6b27
yz41 <2847587055@qq.com>
2022-08-13 20:59:48 +0800

    Initial commit

---
+++ src/main/java/pojo/Content.java
@@ -0,0 +1,14 @@
+package pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Content {
+    private String title;
+    private String time;
+
+}
