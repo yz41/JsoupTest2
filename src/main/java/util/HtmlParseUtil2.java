88e3ea6a4e43219475357799eb60ff25aaf42536
yz41 <2847587055@qq.com>
2022-08-14 11:20:04 +0800

    增加URL

--- src/main/java/util/HtmlParseUtil2.java
+++ src/main/java/util/HtmlParseUtil2.java
@@ -6,26 +6,16 @@ import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.WebResponse;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import net.sf.json.JSONException;
-import net.sf.json.JSONObject;
-import org.apache.commons.lang.StringUtils;
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.SocketTimeoutException;
-import java.net.URL;
-import java.net.URLConnection;
+
 
 public class HtmlParseUtil2 {
     public static void main(String[] args)  {
 
 
         String url = "http://da.wa.news.cn/nodeart/page?nid=11227931&pgnum=1&cnt=10&attr=&tp=1&orderby=1";
-
+        String url2 = "http://da.wa.news.cn/nodeart/page?nid=11227931&pgnum=2&cnt=10&attr=&tp=1&orderby=1";
+        String url3 = "http://da.wa.news.cn/nodeart/page?nid=11227931&pgnum=3&cnt=10&attr=&tp=1&orderby=1";
         final WebClient webClient = new WebClient(BrowserVersion.CHROME);
 
         webClient.getOptions().setThrowExceptionOnScriptError(false);//当JS执行出错的时候是否抛出异常, 这里选择不需要
@@ -38,8 +28,8 @@ public class HtmlParseUtil2 {
         HtmlPage page = null;
         try {
             page = webClient.getPage(url);//尝试加载给出的网页
-            WebResponse webResponse = page.getWebResponse();
-            System.out.println(webResponse);
+            String contentAsString = page.getWebResponse().getContentAsString();
+            System.out.println(contentAsString);
         } catch (Exception e) {
             e.printStackTrace();
         }finally {
@@ -47,8 +37,7 @@ public class HtmlParseUtil2 {
         }
 
         webClient.waitForBackgroundJavaScript(30000);//异步JS执行需要耗时,所以这里线程要阻塞30秒,等待异步JS执行结束
-        WebResponse pageWebResponse = page.getWebResponse();
-        System.out.println(pageWebResponse);
+
 
     }
 }
