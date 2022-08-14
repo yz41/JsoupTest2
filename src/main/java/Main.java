b30f3c3a624229931e0080785b1e7db73cbce9a4
yz41 <2847587055@qq.com>
2022-08-14 12:26:31 +0800

    完成

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,14 +8,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args)  {
-//        String string= new HtmlParseUtil().parseXH().forEach(this.toString()+"\n");
+
 
         try {
             BufferedWriter out = new BufferedWriter(new FileWriter("信息.txt"));
 
-
-
-//            List<Content> contents = new HtmlParseUtil().parseXH();
             List<Content> contents = new MyWebClient().getData();
             StringBuffer stringBuffer = new StringBuffer();
             for (int i = 0; i < contents.size(); i++) {
